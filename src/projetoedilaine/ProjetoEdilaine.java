b468ef550eafc5d0b2d0d5f10d7a6cb6160f5145
AlexEnrique <AlexEnrique@github.com>
2018-09-06 22:41:03 -0300

    Starting the project

---
+++ src/projetoedilaine/ProjetoEdilaine.java
@@ -0,0 +1,13 @@
+package projetoedilaine;
+
+public class ProjetoEdilaine {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Janela0().setVisible(true);
+    }
+    
+}
