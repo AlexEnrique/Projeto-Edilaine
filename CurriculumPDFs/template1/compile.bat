bf9f87629994b2ca27b6b346ef13ca56a8842b1c
AlexEnrique <alexenriquecrispim@hotmail.com>
2018-11-02 09:46:39 -0300

    final v.

---
+++ CurriculumPDFs/template1/compile.bat
@@ -0,0 +1,7 @@
+xelatex C:\CONTRETEC\CurriculumSoftware\CurriculumPDFs\template1\curriculum.tex
+
+move curriculum.pdf C:\Users\%USERNAME%\Desktop
+del curriculum.log curriculum.aux curriculum.pdf
+del C:\CONTRETEC\CurriculumSoftware\tmp\tmp.json
+
+exit
---
+++ CurriculumPDFs/template2/compile.bat
@@ -0,0 +1,7 @@
+xelatex -interaction nonstopmode C:\CONTRETEC\CurriculumSoftware\CurriculumPDFs\template2\curriculum.tex
+
+move curriculum.pdf C:\Users\%USERNAME%\Desktop
+del curriculum.log curriculum.aux curriculum.pdf
+del C:\CONTRETEC\CurriculumSoftware\tmp\tmp.json
+
+exit
